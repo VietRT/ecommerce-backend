e4425738640a94cfa4f9b0204b35f19fad1634e2
Ryan_Truong <ryanhoang32@gmail.com>
2021-11-10 23:50:26 -0600

    test for deploy

--- routes/sql.js
+++ routes/sql.js
@@ -1,5 +1,5 @@
-const path = require('path');
-require('dotenv').config({path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`)});
+// const path = require('path');
+// require('dotenv').config({path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`)});
 const mysql = require('mysql');
 
 const sqlPool = new mysql.createPool({
