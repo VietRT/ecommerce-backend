9e8b6c19bbb3933d5096c65219c0ac3c046e476c
Ryan_Truong <ryanhoang32@gmail.com>
2021-11-11 15:49:13 -0600

    adding try catch to help with environment variable management

--- routes/sql.js
+++ routes/sql.js
@@ -1,5 +1,11 @@
-// const path = require('path');
-// require('dotenv').config({path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`)});
+const path = require('path');
+
+try {
+  require('dotenv').config({path: path.join(__dirname, `../.env.${process.env.NODE_ENV}`)});
+}catch(err) {
+  //dotenv file will not be used in production
+}
+
 const mysql = require('mysql');
 
 const sqlPool = new mysql.createPool({
