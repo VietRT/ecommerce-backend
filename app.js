abeafc01082ed258ee8a968f06b6a003188d0613
Ryan_Truong <ryanhoang32@gmail.com>
2021-12-30 21:28:32 -0600

    adding extra credentials to test setting cookie via heroku

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.set("trust proxy",1);
